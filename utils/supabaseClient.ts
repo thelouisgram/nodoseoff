001751082995da51380d343302b8f8d7f217ba3a
thelouisgram <hadesanoye01@gmail.com>
2023-12-23 13:48:05 +0100

    env

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
-const supabase = createClient('https://opshqmqagtfidynwftzk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wc2hxbXFhZ3RmaWR5bndmdHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEwMTc1MzksImV4cCI6MjAxNjU5MzUzOX0.NCIs69csmiB7ZmA75-HBwrKWqLse3x99eoo0nt_gzTA')
+const supabase = createClient('https://opshqmqagtfidynwftzk.supabase.co', supabaseKey)
 export default supabase

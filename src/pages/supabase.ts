54474a20fde54eede1dd6ab64fcfae522c5861b2
thelouisgram <hadesanoye01@gmail.com>
2024-05-01 02:15:41 +0100

    okkk

--- src/pages/supabase.ts
+++ src/pages/supabase.ts
@@ -1,3 +1,6 @@
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export const supabase = createClientComponentClient()
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+
+export const supabase = createClient('https://opshqmqagtfidynwftzk.supabase.co', supabaseKey)

49d9a43232036bb812310ad2d2ea4d2d17037252
thelouisgram <hadesanoye01@gmail.com>
2024-12-07 07:46:57 +0100

    a fews

--- src/Layout/about/Story.tsx
+++ src/Layout/about/Story.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const Story = () => {
   return (
     <div className="w-full h-auto bg-lightBlue relative pt-[80px]  ">
-      <h1 className="font-bold font-navyBlue text-[36px] text-center mb-12">
+      <h1 className="font-bold font-navyBlue text-[36px] text-center mb-6 md:mb-12">
         About NoDoseOff
       </h1>
       <div
         className="container md:w-[1165px] lg:w-[1165px] mx-auto px-4 xs:px-1 
         ss:px-5 md:px-0 flex flex-col sm:flex-row gap-10 md:gap-20 w-full"
       >
-        <div className="md:w-1/2 h-auto gap-4 flex flex-col text-left md:py-6 font-Inter text-[16px]">
+        <div className="md:w-1/2 h-auto gap-4 flex flex-col text-center md:text-left md:py-6 font-Inter text-[16px]">
           <p>
             What began as a hack-a-thon challenge quickly turned into a personal
             mission for me, fueled by my medical background. In November 2023,
@@ -38,7 +38,7 @@ const Story = () => {
             width={1154}
             height={1519}
             alt="dev"
-            className="w-full sm:w-1/2 md:w-full h-auto"
+            className="w-full sm:w-1/2 md:w-full h-auto rounded-xl"
             src="/assets/dev.jpg"
           />
           <h3 className="text-center font-bold text-[24px] md:text-[30px] mt-4">
